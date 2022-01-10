3352da11962d70726bea2c66d867d0e518ce9f33
chetan360 <97326774+chetan360@users.noreply.github.com>
2022-01-10 18:57:45 +0530

    Update unlock pass.c

--- unlock pass.c
+++ unlock pass.c
@@ -14,27 +14,31 @@ int main()
     scanf("%d",&b);
       if(a==360 && b==007)  
       {
-        {
-        for(j=0;j<=9;j++)
-          if(j==4)
-        {
-          for(j=0;j<=9;j++)
-            if(j==2)
-          {
+      	for(j=0;j<=9;j++)
+          if(j==0)
+          { 
             for(j=0;j<=9;j++)
-              if(j==7)
-            {
-              printf("\nHacked succesfully system is unlocked:");
-            }
+            if(j==4)
+        	{
+          	  for(j=0;j<=9;j++)
+              if(j==2)
+          	   {
+            	 for(j=0;j<=9;j++)
+              	 if(j==7)
+            	 {
+                   printf("Hacked succesfully system is unlocked:");
+            	 }
+          	   }
+         	}
           }
-         }
-        }
       }
   
   }
   else
+  {
     printf("Wrong password..\n");
     printf("Your location is strtessed..\n");
     printf("Alarm is on..\n");
     printf("The system will blast in 5 seconds..");  
   }
+}
