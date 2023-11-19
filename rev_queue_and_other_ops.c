7c9b7e00ca2d157f13de47dae849b6865ee10671
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2023-11-19 18:53:47 +0530

    Update rev_queue_and_other_ops.c

--- rev_queue_and_other_ops.c
+++ rev_queue_and_other_ops.c
@@ -1,47 +1,28 @@
 #include<stdio.h>
 #include"Qlib.h"
+
 int main() {
-    Q q;
-    int n, ch;
+    Queue q;
     init(&q);
-    do {
-        printf("\n\n\n            MENU\n");
-        printf("1. Enqueue\n2. Dqueue.\n3. Display.\n4. Exit.\n");
-        printf("--------------------------------------------\n");
-        printf("Enter your choise : ");
-        scanf("%d", &ch);
 
-        switch(ch) {
-            case 1: if(is_full(&q)) {
-                    printf("Queue is full..");
-                } else {
-                    printf("Enter a number : ");
-                    scanf("%d", &n);
-                    enqueue(&q, n);
-                } break;
-            case 2: if(is_empty(&q)) {
-                    printf("Queue is empty..");
-                } else {
-                    dequeue(&q);
-                } break;
-            case 3: printf("Queue is :\n");
-                for(int i=q.fr+1; i<=q.rr; i++) {
-                    printf("%4d", q.items[i]);
-                } break;
-        }
-    } while(ch != 4);
+    enqueue(&q, 1);
+    enqueue(&q, 2);
+    enqueue(&q, 3);
+    enqueue(&q, 4);
+    enqueue(&q, 5);
 
-    int i, temp;
-    for(i=q.fr+1; i<=(q.rr)/2; i++) {
-        temp = q.items[i];
+    printf("Queue in reverse order.");
+    for(int i=q.fr+1; i<=q.rr/2; i++) {
+        int t = q.items[i];
         q.items[i] = q.items[q.rr-i];
-        q.items[q.rr-i] = temp;
+        q.items[q.rr-i] = t;  
     }
 
-    printf("\nReversed Queue is :\n");
-    for(int i=q.fr+1; i<=q.rr; i++) {
-        printf("%4d", q.items[i]);
-    }
+    printf("\n%d", dqueue(&q));
+    printf("\n%d", dqueue(&q));
+    printf("\n%d", dqueue(&q));
+    printf("\n%d", dqueue(&q));
+    printf("\n%d", dqueue(&q));
 
     return 0;
 }
